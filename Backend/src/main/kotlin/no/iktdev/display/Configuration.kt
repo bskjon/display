3c42b7cfb792397d72ab4a835a8be9a059cb92f2
Brage Skjonborg <brage.skjonborg@hotmail.no>
2023-03-26 18:39:41 +0200

    Updated configuration

--- Backend/src/main/kotlin/no/iktdev/display/Configuration.kt
+++ Backend/src/main/kotlin/no/iktdev/display/Configuration.kt
@@ -1,9 +1,22 @@
 package no.iktdev.display
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 class Configuration {
     companion object {
         val networkAddressForCheck: String? = System.getenv("WAN_ADDRESS") ?: "google.com"
         val platformWattMeterProvider: String? = System.getenv("METER_PLATFORM")
         val platformWattMeterToken: String? = System.getenv("METER_PLATFORM_TOKEN")
+    }
+
+    @Configuration
+    public class WebConfig: WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+        }
     }
 }
