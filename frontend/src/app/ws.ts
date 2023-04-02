51aac9084c47a57c998e5b9ea76c54e4640e76a3
Brage Skjonborg <brage.skjonborg@hotmail.no>
2023-04-02 13:00:01 +0200

    Change + test

--- frontend/src/app/ws.ts
+++ frontend/src/app/ws.ts
@@ -2,7 +2,14 @@ import { IMessage } from "@stomp/stompjs/esm6";
 import { useSubscription } from "react-stomp-hooks";
 
 const deploymentPort = process.env.REACT_APP_DEPLOYMENT_PORT || "8080";
+console.log(`DEPLOYMENT_PORT = ${process.env.REACT_APP_DEPLOYMENT_PORT}`);
 export const wsUrl = `http://localhost:${deploymentPort}/ws`
+
+
+export function getCheckUrl() {
+    const port = process.env.REACT_APP_DEPLOYMENT_PORT || "8080";
+    return `http://localhost:${port}/ws`
+}
 
 export function useWsSubscription<T>(path: string, processData: (data: T) => void,
 ) {
